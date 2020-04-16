e0fce75ff5cf8c1b1d5b35b27c0d9c6f3d6c6f8f
Qifan <plocircle@live.com>
2020-04-16 17:59:50 +0800

    ?

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 check_n_state() {
-  echo "1000 1000 $1" >gen_in
-  ./bin/generator <gen_in >tmp.in
-  ./bin/driver_cuda <tmp.in >> n_obs_1000_n_states_1000
+  echo "1000 1000 $1" > gen.in
+  ./bin/generator < gen.in > tmp.in
+  ./bin/driver_cuda < tmp.in >> n_obs_1000_n_states_1000.out
 
 }
 
